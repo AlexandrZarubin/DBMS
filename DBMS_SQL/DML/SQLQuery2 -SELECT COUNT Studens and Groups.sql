ddaef648cc190c687ee19c270fbd6e85efe9accd
Alexander Zarubin <whitesashka@gmail.com>
2025-02-28 13:38:20 +0500

    DML & Transact-SQL

---
+++ DBMS_SQL/DML/SQLQuery2 -SELECT COUNT Studens and Groups.sql
@@ -0,0 +1,28 @@
+USE VPD_311_Import
+GO
+
+SELECT 
+		direction_name	AS N'Направление обучения',
+		COUNT(group_id) AS N'Количество групп'
+FROM	Groups,		Directions
+WHERE	direction =	direction_id
+GROUP BY direction_name
+ORDER BY N'Количество групп' 
+;
+
+SELECT 
+	[НАправление обучения]	=	direction_name,
+	[Количество студентов]	=	COUNT(stud_id)
+FROM	Students,Groups,Directions
+WHERE	[group]		=	group_id
+AND		direction	=	direction_id
+GROUP BY direction_name;
+
+SELECT DISTINCT 
+		[Направление обучения]	=	direction_name,
+		[Количество групп]		=	COUNT(group_id),
+		[Количество студентов]	=	COUNT(stud_id)
+FROM Students,Groups,Directions
+WHERE	[group]		=	group_id
+AND		direction	=	direction_id
+GROUP BY direction_name,group_id;
---
+++ DBMS_SQL/Tra-SQL/SQLQuery1 - SELECT FROM Schedule.sql
@@ -0,0 +1,15 @@
+USE VPD_311_Import;
+GO
+
+SELECT 
+		[Группа]		=	group_name,	
+		[Дисциплина]	=	discipline_name,
+		[Преподаватель]	=	last_name+' '+first_name+' '+middle_name,
+		[День]			=	DATENAME(WEEKDAY,[date]),
+		[Дата]			=	[date],
+		[Время]			=	[time],
+		[Статус]		=	IIF(spent=1,N'Проведено',N'Запланировано')
+FROM Schedule,Groups,Disciplines,Teachers
+WHERE	[group]			=	group_id
+AND		teacher			=	teacher_id
+AND		discipline		=	discipline_id
